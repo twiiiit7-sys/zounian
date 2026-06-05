a2a8c3c14458fad9398cf153d10a225d3cd09183
DESKTOP-E1RI78Q\academia <stay_goooold@me.com>
2026-06-05 14:20:30 +0900

    render連携

--- src/main/java/com/example/demo/HomeRedirectController.java
+++ src/main/java/com/example/demo/HomeRedirectController.java
@@ -8,11 +8,26 @@ public class HomeRedirectController {
 
     @GetMapping("/")
     public String root() {
-        return "redirect:/index.html";
+        return "forward:/zounian-top/index.html";
     }
 
     @GetMapping("/zounian")
     public String zounian() {
-        return "redirect:/index.html";
+        return "forward:/zounian-top/index.html";
+    }
+
+    @GetMapping("/index.html")
+    public String index() {
+        return "forward:/zounian-top/index.html";
+    }
+
+    @GetMapping("/zounian-top")
+    public String zounianTop() {
+        return "forward:/zounian-top/index.html";
+    }
+
+    @GetMapping("/zounian-top/index.html")
+    public String zounianTopIndex() {
+        return "forward:/zounian-top/index.html";
     }
 }
