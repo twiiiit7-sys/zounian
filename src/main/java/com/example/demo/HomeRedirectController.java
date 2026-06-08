7c55a092e692520bfb6bc13bdcfa852479a93822
DESKTOP-E1RI78Q\academia <stay_goooold@me.com>
2026-06-08 10:22:29 +0900

    renderリンク修正

--- src/main/java/com/example/demo/HomeRedirectController.java
+++ src/main/java/com/example/demo/HomeRedirectController.java
@@ -25,4 +25,39 @@ public class HomeRedirectController {
     public String zounianTop() {
         return "redirect:/zounian-top/index.html";
     }
+
+    @GetMapping({"/concept", "/concept/"})
+    public String concept() {
+        return "redirect:/concept/index.html";
+    }
+
+    @GetMapping({"/guide", "/guide/"})
+    public String guide() {
+        return "redirect:/guide/index.html";
+    }
+
+    @GetMapping({"/menu", "/menu/"})
+    public String menu() {
+        return "redirect:/menu/index.html";
+    }
+
+    @GetMapping({"/news", "/news/"})
+    public String news() {
+        return "redirect:/news/index.html";
+    }
+
+    @GetMapping({"/store", "/store/"})
+    public String store() {
+        return "redirect:/store/index.html";
+    }
+
+    @GetMapping({"/reserve", "/reserve/"})
+    public String reserve() {
+        return "redirect:/reserve/index.html";
+    }
+
+    @GetMapping({"/contact", "/contact/"})
+    public String contact() {
+        return "redirect:/contact/index.html";
+    }
 }
